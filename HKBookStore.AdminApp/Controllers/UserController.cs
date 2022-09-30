b513fefbeb4641f01360e6764e1099d4125f1c0e
duonghoangkha01 <duonghoangkha2001@gmail.com>
2022-09-30 22:25:13 +0700

    Sending message to list using tempdata

--- HKBookStore.AdminApp/Controllers/UserController.cs
+++ HKBookStore.AdminApp/Controllers/UserController.cs
@@ -36,6 +36,10 @@ namespace HKBookStore.AdminApp.Controllers
             
             var data = await _userApiClient.GetUsersPagings(request);
             ViewBag.Keyword = keyword;
+            if (TempData["result"] != null)
+            {
+                ViewBag.SuccessMsg = TempData["result"];
+            }
             return View(data.ResultObj);
         }
 
@@ -59,7 +63,10 @@ namespace HKBookStore.AdminApp.Controllers
                 return View();
             var result = await _userApiClient.RegisterUser(request);
             if (result.IsSuccessed)
+            {
+                TempData["result"] = "Thêm mới người dùng thành công";
                 return RedirectToAction("Index");
+            }
 
             ModelState.AddModelError("", result.Message);
             return View(request);
@@ -94,7 +101,10 @@ namespace HKBookStore.AdminApp.Controllers
 
             var result = await _userApiClient.UpdateUser(request.Id, request);
             if (result.IsSuccessed)
+            {
+                TempData["result"] = "Cập nhật người dùng thành công";
                 return RedirectToAction("Index");
+            }
 
             ModelState.AddModelError("", result.Message);
             return View(request);
@@ -126,7 +136,10 @@ namespace HKBookStore.AdminApp.Controllers
 
             var result = await _userApiClient.Delete(request.Id);
             if (result.IsSuccessed)
+            {
+                TempData["result"] = "Xóa người dùng thành công";
                 return RedirectToAction("Index");
+            }
 
             ModelState.AddModelError("", result.Message);
             return View(request);
